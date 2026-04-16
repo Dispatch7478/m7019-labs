379c8572f2a5bae8949dc2156bb678608a4515e2
dispatch <nahtse-2@student.ltu.se>
2026-04-16 14:47:23 +0200

    ui for movie item card + homepage displaying two lists (popular and top rated)

--- app/src/main/java/com/example/moviedb/ui/components/CommonUi.kt
+++ app/src/main/java/com/example/moviedb/ui/components/CommonUi.kt
@@ -1,50 +1,96 @@
 package com.example.moviedb.ui.components
 
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil3.compose.AsyncImage
+import com.example.moviedb.data.Movies
+import com.example.moviedb.models.Movie
+import com.example.moviedb.ui.theme.MovieDBTheme
 
+private const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/"
+private const val POSTER_IMAGE_BASE_WIDTH = "w500"
+private const val BACKDROP_IMAGE_BASE_WIDTH = "w1280"
 @Composable
-fun MoviePreviewCard(
-    title :String,
-    description: String,
-    imageResource: Int,
+fun MovieListItemCard(
+    movie : Movie,
     modifier : Modifier = Modifier
     ){
-    Card(
+    ElevatedCard(
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp
+        ),
         modifier = modifier
-            .width(260.dp)
+            .width(350.dp)
             .padding(8.dp),
         shape = RoundedCornerShape(12.dp)
     ) {
-        Row() {
-            Image(
-                painter = painterResource(imageResource),
-                contentDescription = null
+        Row(
+            modifier = Modifier
+                .padding(4.dp)
+        ) {
+            AsyncImage(
+                model = IMAGE_BASE_URL + POSTER_IMAGE_BASE_WIDTH +  movie.posterPath,
+                contentDescription = movie.title,
+                modifier = Modifier
+                    .width(175.dp)
             )
-
-            Column() {
+            Spacer(modifier = Modifier.width(4.dp))
+            // Async image here
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .width(200.dp)
+            ) {
                 Text(
-                    text = title
+                    text = movie.title,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold
                 )
                 Spacer(modifier = Modifier.height(4.dp))
-
                 Text(
-                    text = description,
+                    text = movie.overview,
                     maxLines = 3,
+                    fontSize = 15.sp
+                )
+                Spacer(modifier = Modifier.height(50.dp))
+                Text(
+                    text = "Released: " + movie.releaseDate,
+                    textAlign = TextAlign.End
                 )
             }
         }
+    }
+}
+
+@Preview
+@Composable
+fun HomePagePreview(){
+    MovieDBTheme() {
+        MovieListItemCard(
+            Movies().getTopRatedMovies().first(),
+            modifier = Modifier
+                .fillMaxSize()
+        )
     }
 }
--- app/src/main/java/com/example/moviedb/ui/screens/HomePage.kt
+++ app/src/main/java/com/example/moviedb/ui/screens/HomePage.kt
@@ -1,13 +1,26 @@
 package com.example.moviedb.ui.screens
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.moviedb.data.Movies
+import com.example.moviedb.models.Movie
+import com.example.moviedb.ui.components.MovieListItemCard
 import com.example.moviedb.ui.theme.MovieDBTheme
 
 
@@ -19,30 +32,66 @@ fun HomePage(
 ){
     Column(
         modifier = modifier
+            .verticalScroll(rememberScrollState())
     ) {
-        Text(text="displays 3 horizontal scrollable lists (popular, top rated, favorites")
-        Button(
-            onClick = onViewMoreClicked
-        ) {
-            Text(text = "View more list")
-        }
+        HorizontalMoviesListPreview(
+            listName = "Popular Movies",
+            movieList = Movies().getPopularMovies(),
+            onViewMoreClicked = {},
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        HorizontalMoviesListPreview(
+            listName = "Top Rated Movies",
+            movieList = Movies().getTopRatedMovies(),
+            onViewMoreClicked = {},
+        )
+        // If any favorite here
     }
 }
 
 @Composable
 fun HorizontalMoviesListPreview(
-    onViewMoreClicked: () -> Unit
+    listName : String,
+    movieList : List<Movie>,
+    onViewMoreClicked: () -> Unit,
+    modifier: Modifier = Modifier
 ){
-    LazyRow() {
-        // List of movie cards
-    }
-    Button(
-        onClick = onViewMoreClicked
+    Column(
+        modifier = modifier
+            .height(400.dp)
+            .padding(8.dp)
     ) {
-        Text(text = "View More")
+        Text(
+            text = listName,
+            fontSize = 30.sp,
+            fontWeight = FontWeight.ExtraBold
+        )
+        Spacer(
+            modifier = Modifier.height(8.dp)
+        )
+
+        LazyRow(
+            modifier = Modifier.height(300.dp)
+        ) {
+            items(movieList.take(6)){ movie ->
+                MovieListItemCard(
+                    movie,
+                    modifier = Modifier.padding(8.dp)
+                )
+            }
+        }
+        Button(
+            onClick = onViewMoreClicked,
+            modifier = Modifier
+                .align(Alignment.End)
+        ) {
+            Text(
+                text = "View More",
+                fontSize = 15.sp
+            )
+        }
     }
 }
-
 
 @Preview
 @Composable
